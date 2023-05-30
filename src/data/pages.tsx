115531d8a88797ea00bc29db40a6f5452ba8b1fe
dot-1x <mugiwaraluffy2003@gmail.com>
2023-05-30 14:39:14 +0700

    Changed home to card, added new Hashirama Sage Mode

--- src/data/pages.tsx
+++ src/data/pages.tsx
@@ -2,16 +2,16 @@ export const listPage = [
     {
         "href": "/deploy",
         "name": "Deploy",
-        "desc": "Tools untuk deploy seperti dalam game"
-    },
-    {
-        "href": "/ninja",
-        "name": "Ninja",
-        "desc": "List semua ninja yang ada dalam game"
+        "desc": "In-Game like deploy tools\nCan also be used to fix pipes\nCalculate differences between 2 combos",
     },
     {
         "href": "/combo",
         "name": "Combo",
-        "desc": "List semua combo yang ada dalam game"
+        "desc": "All available combo\nCalculate your own combo with max ninjas 15"
+    },
+    {
+        "href": "/ninja",
+        "name": "Ninja",
+        "desc": "All current available ninjas\nCalculate Quality for each ninja"
     },
 ]
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
 import Content from '@/components/content.component'
 import Head from 'next/head'
-import { Container, ListGroup } from 'react-bootstrap'
-import { ListItemLink } from '@/components/menu.component'
+import { Card, Col, Container, Row } from 'react-bootstrap'
 import { listPage } from '@/data/pages'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -12,11 +12,31 @@ export default function Home() {
       </Head>
       <Content>
         <Container>
-          <h1>NH: New Era Tools</h1>
-          <p>Tools utilitas game Ninja Heroes New Era</p>
-          <p>Fitur tersedia:</p>
+          <h1 className='text-center'>NH: New Era Tools</h1>
+          <p className='text-center'>Utility tools for Ninja Heroes: New Era</p>
           <Container className="border-top p-2">
-            <ListGroup variant='flush'>
+            <Row lg={2} md={1} className="g-4">
+              {
+                listPage.map((v, i) => (
+                <Col key={v.name}>
+                  <Link href={`${v.href}`} style={{textDecoration: "none"}} className="text-white">
+                    <Card bg="secondary" border="info">
+                        <Card.Img src={`assets/card/${v.name.toLocaleLowerCase()}.png`} />
+                      <Card.Header>
+                        {v.name}
+                      </Card.Header>
+                      <Card.Body>
+                        <Card.Text style={{whiteSpace: "pre-line"}}>
+                          {v.desc}
+                        </Card.Text>
+                        </Card.Body>
+                      </Card>
+                  </Link>
+                </Col>
+                ))
+              }
+            </Row>
+            {/* <ListGroup variant='flush'>
               {
                 listPage.map(
                   (v, i) => (
@@ -24,7 +44,7 @@ export default function Home() {
                   )
                 )
               }
-            </ListGroup>
+            </ListGroup> */}
           </Container>
         </Container>
       </Content>
