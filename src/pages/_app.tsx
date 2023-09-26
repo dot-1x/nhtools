245992c1f2a3e523e4965260f3403a2f44071531
dot-1x <mugiwaraluffy2003@gmail.com>
2023-09-26 13:14:08 +0700

    update packages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
-import type { AppProps } from 'next/app'
-import Script from 'next/script'
+import type { AppProps } from "next/app"
+import Script from "next/script"
 import "bootstrap/dist/css/bootstrap.css"
 import "../style/style.css"
 
@@ -7,7 +7,11 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
-      <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossOrigin="anonymous"></Script>
+      <Script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
+        crossOrigin="anonymous"
+      ></Script>
     </>
   )
 }
