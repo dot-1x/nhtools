4a685ced6c79aecb394294405af924b5023894b1
dot-1x <mugiwaraluffy2003@gmail.com>
2023-10-26 00:36:47 +0700

    fix: fix some text, format code with prettier

--- src/components/ui-elements/donate.ui.tsx
+++ src/components/ui-elements/donate.ui.tsx
@@ -1,6 +1,6 @@
-import Link from 'next/link';
-import { Button, Col, Container, Row } from 'react-bootstrap';
-import { Image } from 'react-bootstrap';
+import Link from "next/link"
+import { Button, Col, Container, Row } from "react-bootstrap"
+import { Image } from "react-bootstrap"
 
 export default function Donate() {
   return (
@@ -8,18 +8,34 @@ export default function Donate() {
       <Container>
         <Row className="d-flex flex-column flex-lg-row justify-content-lg-center   ">
           <Col className="my-3 align-self-center donate__image">
-            <Image src="/assets/bg/w1.png" alt="sasuke" width={470} className="align-items-start" />
+            <Image
+              src="/assets/bg/w1.png"
+              alt="sasuke"
+              width={470}
+              className="align-items-start"
+            />
             <div>
-              <Image src="/assets/bg/w9.png" alt="sasuke" width={470} className="align-items-end" />
+              <Image
+                src="/assets/bg/w9.png"
+                alt="sasuke"
+                width={470}
+                className="align-items-end"
+              />
             </div>
           </Col>
           <Col className="my-3   d-lg-block align-self-center">
             <h2 className="fs-1">DONATE TO SUPPORT</h2>
-            <p>Mempunyai pengertian setiap baris data pada tabel pertama dihubungkan hanya ke satu baris data pada tabel ke dua. Hubungan antara file pertama dan file kedua adalah satu satu. Hubungan antara file Hubungan .</p>
+            <p>Setiap donasi berharga untuk support pengembang</p>
             <div className="w-auto d-flex flex-row mt-5">
-              <Link href={'https://teer.id/dotcchi1x'} className="text-decoration-none" target="_blank">
+              <Link
+                href={"https://teer.id/dotcchi1x"}
+                className="text-decoration-none"
+                target="_blank"
+              >
                 <Button variant="primary" className="border-2  mb-5 me-3">
-                  <span className=" px-4 px-lg-5 text-capitalize fs-5 fw-semibold ">DONATE ME</span>
+                  <span className=" px-4 px-lg-5 text-capitalize fs-5 fw-semibold ">
+                    DONATE ME
+                  </span>
                 </Button>
               </Link>
             </div>
@@ -27,5 +43,5 @@ export default function Donate() {
         </Row>
       </Container>
     </section>
-  );
+  )
 }
--- src/components/ui-elements/header.ui.tsx
+++ src/components/ui-elements/header.ui.tsx
@@ -1,6 +1,6 @@
-import Link from 'next/link';
-import { Button, Col, Container, Row } from 'react-bootstrap';
-import { Image } from 'react-bootstrap';
+import Link from "next/link"
+import { Button, Col, Container, Row } from "react-bootstrap"
+import { Image } from "react-bootstrap"
 
 export default function Header() {
   return (
@@ -11,16 +11,27 @@ export default function Header() {
             <h1>
               NH <span>NEW ERA TOOL’S</span>
             </h1>
-            <p>Mempunyai pengertian setiap baris data data . </p>
+            <p>Utility tools for NH: New Era game</p>
             <div className="w-auto d-flex flex-row mt-5">
-              <Link href={'/#tools'} className="text-decoration-none">
+              <Link href={"/#tools"} className="text-decoration-none">
                 <Button variant="primary" className="border-2  mb-5 me-3 ">
-                  <span className=" px-4 px-lg-5 text-capitalize fs-5 fw-semibold">GET STARTED</span>
+                  <span className=" px-4 px-lg-5 text-capitalize fs-5 fw-semibold">
+                    GET STARTED
+                  </span>
                 </Button>
               </Link>
-              <Link href={'https://www.ninjaheroesnewera.com/'} className="text-decoration-none" target="_blank">
-                <Button variant="outline-primary" className="border-2  mb-5 me-3 d-none d-md-block">
-                  <span className=" px-4 px-lg-5  fs-5 fw-semibold">INSTALL GAME</span>
+              <Link
+                href={"https://www.ninjaheroesnewera.com/"}
+                className="text-decoration-none"
+                target="_blank"
+              >
+                <Button
+                  variant="outline-primary"
+                  className="border-2  mb-5 me-3 d-none d-md-block"
+                >
+                  <span className=" px-4 px-lg-5  fs-5 fw-semibold">
+                    INSTALL GAME
+                  </span>
                 </Button>
               </Link>
             </div>
@@ -31,5 +42,5 @@ export default function Header() {
         </Row>
       </Container>
     </section>
-  );
+  )
 }
