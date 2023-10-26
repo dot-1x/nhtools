f3ef4f43e54f7c38348d4774f6c6ff1ea93a52d8
dot-1x <mugiwaraluffy2003@gmail.com>
2023-10-27 01:12:26 +0700

    fix: remove params

--- src/components/ninja/ninjaChoose.component.tsx
+++ src/components/ninja/ninjaChoose.component.tsx
@@ -1,13 +1,19 @@
 import { useDraggable } from "@dnd-kit/core"
 import { getAllNinja } from "@/utils/ninja.utils"
 import { NinjaImage } from "./ninjaImage.component"
-import React, { ReactNode, useState } from 'react';
-import { SelectNinjaButton } from '../deploy.component';
+import React, { ReactNode, useState } from "react"
+import { SelectNinjaButton } from "../deploy.component"
 
-export function DragNinja({ children, id }: { children: ReactNode; id: string }) {
+export function DragNinja({
+  children,
+  id,
+}: {
+  children: ReactNode
+  id: string
+}) {
   const { attributes, listeners, setNodeRef } = useDraggable({
     id: id,
-  });
+  })
 
   return (
     <div
@@ -20,17 +26,17 @@ export function DragNinja({ children, id }: { children: ReactNode; id: string })
     >
       {children}
     </div>
-  );
+  )
 }
 
 export function ChooseNinja() {
-  const [CHOOSED, setChoosed] = useState('SSS');
-  const found = [...getAllNinja()].filter((v) => v.kelas === CHOOSED);
+  const [CHOOSED, setChoosed] = useState("SSS")
+  const found = [...getAllNinja()].filter((v) => v.kelas === CHOOSED)
   return (
     <>
       {found && (
         <>
-          <h2 className="fs-1 text-uppercase" style={{ letterSpacing: '1px' }}>
+          <h2 className="fs-1 text-uppercase" style={{ letterSpacing: "1px" }}>
             List Ninja
           </h2>
           <div className="bg-soft-dark-primary-3 p-3 p-md-4 me-md-2 rounded mb-2 mb-md-5">
@@ -38,13 +44,13 @@ export function ChooseNinja() {
             <div
               className="overflow-x-auto overflow-y-hidden p-1 d-flex flex-row pb-4 "
               onScroll={(ev) => {
-                ev.preventDefault();
+                ev.preventDefault()
               }}
             >
               {found.map((v) => (
                 <div className="mx-2" key={v.name}>
-                  <DragNinja id={v.name.replaceAll(' ', '-')}>
-                    <NinjaImage key={v.name} name={v.name} tColor={v.attribute.atas} rColor={v.attribute.kanan} bColor={v.attribute.bawah} lColor={v.attribute.kiri} />
+                  <DragNinja id={v.name.replaceAll(" ", "-")}>
+                    <NinjaImage key={v.name} name={v.name} />
                   </DragNinja>
                 </div>
               ))}
@@ -53,5 +59,5 @@ export function ChooseNinja() {
         </>
       )}
     </>
-  );
+  )
 }
